8ef6bb818819b69cadee1d8e9fc43ac1bc25170b
henrique-miyamoto-deel <henrique.miyamoto@deel.com>
2024-04-21 18:08:51 -0300

    update branch

--- ling_prog/202404-teste.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Digite um número inteiro: "))  # assumindo que esta linha é para pedir entrada ao usuário
-while n > 0:
-    print(n)
-    n -= 1  # equivalente a n = n - 1
-print("Tempo esgotado!")
